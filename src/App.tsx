f45b226f4a468ffcf96cc5b27f1b899482bda480
Nabeel <nabeel92914@outlook.com>
2024-04-18 12:26:21 +0100

    added an animated background

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
 import './App.scss'
+import NavigationMenu from './components/NavigationMenu/NavigationMenu';
+
 
 function App() {
 
   return (
     <>
-      
+      <section className='navigationMenu'>
+        <NavigationMenu />
+      </section>
+
+      <section className='mainContent'>
+        
+      </section>
+
+        <div className="bubbles">
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+        </div>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavigationMenu = () => {
+  return (
+    <h2>Hello World</h2>
+  )
+}
+
+export default NavigationMenu;
